e46fa56ecedc06f959b29fc56369474e9c58be81
ianpareek <pareekian@gmail.com>
2026-07-06 18:14:27 -0400

    Add files via upload

---
+++ mean value2.py
@@ -0,0 +1,13 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_numbers = 40
+
+sum = mean1 * total_numbers
+print(sum)
+
+correct_sum = (sum - wrong_number) + correct_number
+print("The correct sum is:", correct_sum)
+
+mean2 = correct_sum / total_numbers
+print("The correct mean is:", mean2)
